30c82f761c765a9e135d9103746f60b26878718a
FranciscoJavierHM <61300292+Francisco-Javier-Herrejon-Mendoza@users.noreply.github.com>
2022-07-26 13:10:56 -0500

    Update settings.py
    debug

--- CRUD_Django/settings.py
+++ CRUD_Django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0sfi5l(xe6&y0&z#vw7l09@s=ek!8=*#1^umu#yp+)&pri#h_*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
